e66da9508bc5129026ca42c6da19c3dcc19a8f6d
meghakaradikoppa <meghakaradikoppa@gamil.com>
2025-04-24 11:10:45 +0530

    save

--- demo.java
+++ demo.java
@@ -4,8 +4,12 @@ public class demo {
         int b = 20;
         int sum = a + b;
         int sub = a - b;
+        int mult = a * b;
+        int div= a / b;
 
         System.out.println("sum is=" + sum);
         System.out.println("sub is=" + sub);
+        System.out.println("mult is=" + mult);
+        System.out.println("div is=" + div);
     }
 }
